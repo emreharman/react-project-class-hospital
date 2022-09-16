0016a627567c5736f4a801c755323c8f14cee366
emreharman <emre.harman@ogr.sakarya.edu.tr>
2022-09-16 23:18:19 +0300

    1week routing&header&listing randevular

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Hastalar from "./pages/Hastalar";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hastalar" element={<Hastalar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Hastalar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Hastalar = (props) => {
+  return (
+    <div>
+      <Header />
+      <h1>Hastalar sayfası</h1>
+    </div>
+  );
+};
+
+export default Hastalar;
