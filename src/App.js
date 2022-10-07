cfb6fd106eeea0780bef151f6ffc0d495b4d9e80
emreharman <emrehrmnb@gmail.com>
2022-10-08 00:02:28 +0300

    addPatient&deletePatient

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Hastalar from "./pages/Hastalar";
+import HastaEkle from "./pages/HastaEkle";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hastalar" element={<Hastalar />} />
+        <Route path="/hasta-ekle" element={<HastaEkle />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Hastalar.js
+++ src/pages/Hastalar.js
@@ -1,11 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import Header from "../components/Header";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 const Hastalar = (props) => {
+  const navigate = useNavigate();
+  const [hastalar, setHastalar] = useState(null);
+  const [updateComponent,setUpdateComponent]=useState(false)
+  useEffect(() => {
+    axios
+      .get("http://localhost:3004/hastalar")
+      .then((res) => {
+        setHastalar(res.data);
+      })
+      .catch((err) => console.log("Hastalar page getHastalarErr", err));
+  }, [updateComponent]);
+
+
+  const handleDeleteHasta=(hasta)=>{
+    console.log(hasta)
+    axios.delete(`http://localhost:3004/hastalar/${hasta.id}`)
+    .then(deleteHastaRes=>{
+      hasta.islemIds.map(islemId=>{
+        axios.delete(`http://localhost:3004/islemler/${islemId}`)
+        .then(islemDeleteRes=>{
+
+        })
+        .catch(err=>console.log("hastalar sayfası deleteIslem err",err))
+      })
+      setUpdateComponent(!updateComponent)
+    })
+    .catch(err=>console.log("hasatalar sayfası hastaDelete err",err))
+  }
+
+  if (hastalar === null) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
       <Header />
-      <h1>Hastalar sayfası</h1>
+
+      <TableContainer style={{ marginTop: "50px" }} component={Paper}>
+        <div
+          style={{
+            marginBottom: "20px",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}>
+          <Button onClick={() => navigate("/hasta-ekle")} variant="contained">
+            Hasta Ekle
+          </Button>
+        </div>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead sx={{ backgroundColor: "#999" }}>
+            <TableRow>
+              <TableCell>Adı</TableCell>
+              <TableCell>Soyadı</TableCell>
+              <TableCell>Telefon Numarası</TableCell>
+              <TableCell>İşlem</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              hastalar.length === 0 && (
+                <TableRow  >
+                  <TableCell align="center" colSpan={4}>Kayıtlı Hasta Bulunmamaktadır</TableCell>
+                </TableRow>
+              )
+            }
+            {hastalar.map((hasta) => (
+              <TableRow
+                key={hasta.id}
+                sx={{
+                  "&:last-child td, &:last-child th": { border: 0 },
+                }}>
+                <TableCell>{hasta.name}</TableCell>
+                <TableCell>{hasta.surname}</TableCell>
+                <TableCell>{hasta.phone}</TableCell>
+                <TableCell>
+                  <Stack spacing={2} direction="row">
+                    <Button variant="outlined" color="primary">Düzenle</Button>
+                    <Button onClick={()=>handleDeleteHasta(hasta)} variant="outlined" color="error">Sil</Button>
+                    <Button variant="outlined" color="secondary">Detaylar</Button>
+                  </Stack>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
